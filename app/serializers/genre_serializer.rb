259ad881ddafa69ed4c639fd2f0d643db621ca2d
treuther <tracyreuther@gmail.com>
2020-03-04 04:53:44 -0600

    created genre and movie serializers

--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,3 +1,7 @@
 class GenreSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name
+
+  # When a request is made, the backend will check here FIRST! That's why you have to add the has_many
+
+  has_many :movies
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :genre_id, :title, :rating, :description
 end
+
+# genre_id is not needed because the movie route is nested with genre.
+# if this changes, then genre_id will be needed here.
