b4ace75603a65f39ddab63ac49b0b5419244fc1d
treuther <tracyreuther@gmail.com>
2020-03-10 12:57:33 -0500

    minor change to  check inputs for title uniqueness

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,17 +5,17 @@ class Movie < ApplicationRecord
     # accepts_nested_attributes_for :genre #allows user to create a genre while creating a new movie
 
     validates :title, :rating, :description, presence: true
-    validates :title, uniqueness, true
+    validates :title, uniqueness: true
     validates_inclusion_of :rating, :in => ['R', 'PG-13', 'PG']
 
     # if movie title already exists, cannot save again.
 
-    def check_dupes(movie)
-        if self.title == movie.title
-            return 'Movie title already exists.'
-        else
-            self.save
-        end
-    end
+    # def check_dupes(movie)
+    #     if self.title == movie.title
+    #         return 'Movie title already exists.'
+    #     else
+    #         self.save
+    #     end
+    # end
     
 end
