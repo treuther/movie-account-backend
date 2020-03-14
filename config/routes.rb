0f4a8502c3041c2081db44573fd8265dfe0a26ee
treuther <tracyreuther@gmail.com>
2020-03-14 10:40:09 -0500

    added nested rooute of notes to movies

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :genres do
-        resources :movies
+        resources :movies do
+          resources :notes
+        end
       end
     end
   end
